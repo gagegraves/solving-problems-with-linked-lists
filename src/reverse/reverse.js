61963dc4d6f99d66af255ee15466944aadeaef1e
Gage Graves <gagegraves97+github@gmail.com>
2021-09-25 13:00:25 -0700

    intial commit

---
+++ src/reverse/reverse.js
@@ -0,0 +1,43 @@
+const LinkedList = require("../lib/linkedList");
+
+/**
+ * Iteratively reverse a linked list
+ *
+ * @param list
+ * A (potentially empty) linked list
+ *
+ * @returns {LinkedList}
+ * A linked list in reverse order
+ */
+function reverseIterative(list) {
+    const reversedList = new LinkedList();
+    let node = list.head;
+    while(node) {
+        reversedList.insertAtHead(node.value);
+        node = node.next;
+    }
+    return reversedList;
+}
+
+/**
+ * Recursively reverse a linked list
+ *
+ * @param list
+ * A (potentially empty) linked list
+ *
+ * @returns {LinkedList}
+ * A linked list in reverse order
+ */
+ function reverseRecursive(list) {
+    if (list.length <= 1) {
+      return list;
+    }
+  
+    const head = list.head.value;
+    list.remove((node, index) => index === 0);
+    const reversedList = reverseRecursive(list);
+    reversedList.insert(head);
+    return reversedList;
+  }
+
+module.exports = { reverseIterative, reverseRecursive };
---
+++ src/thirdfromend/thirdfromend.js
@@ -0,0 +1,34 @@
+const LinkedList = require("../lib/linkedList");
+
+/**
+ * Return the third element from the end of the linked list.
+ *
+ * @param {LinkedList} list
+ *
+ * @returns {Node} the third element from the end of the list,
+ * null if list length < 3
+ *
+ */
+function thirdFromEnd(list) {
+  if (!list.head) return null;
+
+  let pointer1 = list.head;
+  let i = 0;
+
+  while (i < 2 && pointer1.next != null) {
+    pointer1 = pointer1.next;
+    i++;
+  }
+
+  if (i < 2) return null;
+  let pointer2 = list.head;
+
+  while (pointer1.next != null) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
+
+  return pointer2;
+}
+
+module.exports = thirdFromEnd;
